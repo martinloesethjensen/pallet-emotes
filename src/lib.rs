66b78d0ca58657b7a3b3fa911b68e6f4a668f6be
Alex Plutta <alex.plutta@googlemail.com>
2021-09-29 20:30:48 +0200

    add basic test setup

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,8 @@ pub mod pallet {
     }
 
     #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+
     pub enum Event<T: Config> {
         //RMRK_Example_Event(T::AccountId),
     }
@@ -50,5 +52,107 @@ pub mod pallet {
         pub fn hello_rmrk(origin: OriginFor<T>) -> DispatchResult {
             Ok(())
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate as pallet_emotes;
+    use frame_support::{assert_ok, ord_parameter_types, parameter_types, traits::GenesisBuild};
+    use frame_system::EnsureSignedBy;
+    use sp_core::H256;
+    use sp_runtime::{
+        testing::Header,
+        traits::{BadOrigin, BlakeTwo256, IdentityLookup},
+    };
+
+    type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+    type Block = frame_system::mocking::MockBlock<Test>;
+
+    frame_support::construct_runtime!(
+        pub enum Test where
+        Block = Block,
+        NodeBlock = Block,
+        UncheckedExtrinsic = UncheckedExtrinsic,
+        {
+			System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+			Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+			Emotes: pallet_emotes::{Pallet, Call, Storage, Event<T>},
+        }
+    );
+
+    parameter_types! {
+        pub const BlockHashCount: u64 = 250;
+        pub BlockWeights: frame_system::limits::BlockWeights =
+            frame_system::limits::BlockWeights::simple_max(1024);
+    }
+
+    impl frame_system::Config for Test {
+        type BaseCallFilter = frame_support::traits::Everything;
+        type BlockWeights = ();
+        type BlockLength = ();
+        type DbWeight = ();
+        type Origin = Origin;
+        type Index = u64;
+        type BlockNumber = u64;
+        type Hash = H256;
+        type Call = Call;
+        type Hashing = BlakeTwo256;
+        type AccountId = u64;
+        type Lookup = IdentityLookup<Self::AccountId>;
+        type Header = Header;
+        type Event = Event;
+        type BlockHashCount = BlockHashCount;
+        type Version = ();
+        type PalletInfo = PalletInfo;
+        type AccountData = pallet_balances::AccountData<u64>;
+        type OnNewAccount = ();
+        type OnKilledAccount = ();
+        type SystemWeightInfo = ();
+        type SS58Prefix = ();
+        type OnSetCode = ();
+    }
+
+    parameter_types! {
+        pub const ExistentialDeposit: u64 = 1;
+    }
+
+    impl pallet_balances::Config for Test {
+        type MaxLocks = ();
+        type MaxReserves = ();
+        type ReserveIdentifier = [u8; 8];
+        type Balance = u64;
+        type Event = Event;
+        type DustRemoval = ();
+        type ExistentialDeposit = ExistentialDeposit;
+        type AccountStore = System;
+        type WeightInfo = ();
+    }
+
+    parameter_types! {
+        pub const ReservationFee: u64 = 2;
+        pub const MinLength: u32 = 3;
+        pub const MaxLength: u32 = 16;
+    }
+    ord_parameter_types! {
+        pub const One: u64 = 1;
+    }
+
+    impl Config for Test {
+        type Currency = Balances;
+        type Event = Event;
+    }
+    fn new_test_ext() -> sp_io::TestExternalities {
+		let mut t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+		pallet_balances::GenesisConfig::<Test> { balances: vec![(1, 10), (2, 10)] }
+			.assimilate_storage(&mut t)
+			.unwrap();
+		t.into()
+	}
+    #[test]
+    fn test() {
+        println!("it works");
+        assert_eq!(true, true);
     }
 }
