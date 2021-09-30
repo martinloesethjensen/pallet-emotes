4ee082d4f7e01b810d666f18eb3482c4e8fee424
Alex Plutta <alex.plutta@googlemail.com>
2021-09-30 22:05:43 +0200

    use emotes module in pallet

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@
 use frame_support::traits::{Currency, ReservableCurrency};
 pub use pallet::*;
 use sp_std::prelude::*;
+mod emotes;
+mod tests;
 
 type BalanceOf<T> =
     <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
@@ -12,7 +14,7 @@ type BalanceOf<T> =
 #[frame_support::pallet]
 pub mod pallet {
     use super::*;
-    use frame_support::pallet_prelude::*;
+    use frame_support::{decl_error, pallet_prelude::*};
     use frame_system::pallet_prelude::*;
 
     #[pallet::config]
@@ -35,6 +37,7 @@ pub mod pallet {
     #[pallet::error]
     pub enum Error<T> {
         //EventName
+        InvalidInput,
     }
 
     /// The lookup table for names.
@@ -50,109 +53,7 @@ pub mod pallet {
     impl<T: Config> Pallet<T> {
         #[pallet::weight(50_000_000)]
         pub fn hello_rmrk(origin: OriginFor<T>) -> DispatchResult {
-            Ok(())
+            Err(Error::<T>::InvalidInput)?
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate as pallet_emotes;
-    use frame_support::{assert_ok, ord_parameter_types, parameter_types, traits::GenesisBuild};
-    use frame_system::EnsureSignedBy;
-    use sp_core::H256;
-    use sp_runtime::{
-        testing::Header,
-        traits::{BadOrigin, BlakeTwo256, IdentityLookup},
-    };
-
-    type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
-    type Block = frame_system::mocking::MockBlock<Test>;
-
-    frame_support::construct_runtime!(
-        pub enum Test where
-        Block = Block,
-        NodeBlock = Block,
-        UncheckedExtrinsic = UncheckedExtrinsic,
-        {
-			System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
-			Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
-			Emotes: pallet_emotes::{Pallet, Call, Storage, Event<T>},
-        }
-    );
-
-    parameter_types! {
-        pub const BlockHashCount: u64 = 250;
-        pub BlockWeights: frame_system::limits::BlockWeights =
-            frame_system::limits::BlockWeights::simple_max(1024);
-    }
-
-    impl frame_system::Config for Test {
-        type BaseCallFilter = frame_support::traits::Everything;
-        type BlockWeights = ();
-        type BlockLength = ();
-        type DbWeight = ();
-        type Origin = Origin;
-        type Index = u64;
-        type BlockNumber = u64;
-        type Hash = H256;
-        type Call = Call;
-        type Hashing = BlakeTwo256;
-        type AccountId = u64;
-        type Lookup = IdentityLookup<Self::AccountId>;
-        type Header = Header;
-        type Event = Event;
-        type BlockHashCount = BlockHashCount;
-        type Version = ();
-        type PalletInfo = PalletInfo;
-        type AccountData = pallet_balances::AccountData<u64>;
-        type OnNewAccount = ();
-        type OnKilledAccount = ();
-        type SystemWeightInfo = ();
-        type SS58Prefix = ();
-        type OnSetCode = ();
-    }
-
-    parameter_types! {
-        pub const ExistentialDeposit: u64 = 1;
-    }
-
-    impl pallet_balances::Config for Test {
-        type MaxLocks = ();
-        type MaxReserves = ();
-        type ReserveIdentifier = [u8; 8];
-        type Balance = u64;
-        type Event = Event;
-        type DustRemoval = ();
-        type ExistentialDeposit = ExistentialDeposit;
-        type AccountStore = System;
-        type WeightInfo = ();
-    }
-
-    parameter_types! {
-        pub const ReservationFee: u64 = 2;
-        pub const MinLength: u32 = 3;
-        pub const MaxLength: u32 = 16;
-    }
-    ord_parameter_types! {
-        pub const One: u64 = 1;
-    }
-
-    impl Config for Test {
-        type Currency = Balances;
-        type Event = Event;
-    }
-    fn new_test_ext() -> sp_io::TestExternalities {
-		let mut t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
-		pallet_balances::GenesisConfig::<Test> { balances: vec![(1, 10), (2, 10)] }
-			.assimilate_storage(&mut t)
-			.unwrap();
-		t.into()
-	}
-    #[test]
-    fn test() {
-        println!("it works");
-        assert_eq!(true, true);
     }
 }
