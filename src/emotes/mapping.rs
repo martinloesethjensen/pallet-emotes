9524ef0aecc5e85a31ae55f3fac76d0bfc786763
Martin <martinloesethjensen@gmail.com>
2021-10-05 00:23:25 +0200

    mappings of emojis unicode from binary + tests

--- src/emotes/mapping.rs
+++ src/emotes/mapping.rs
@@ -1,65 +1,47 @@
 use std::collections::HashMap;
 
-use emojis::Emoji;
-
 // TODO: store binary instead of String as key
-fn populate_emojis() -> HashMap<String, String> {
+fn populate_emojis() -> HashMap<u128, String> {
     let mut mapping = HashMap::new();
-    mapping.insert(String::from("1"), String::from("1f600"));
-    mapping.insert(String::from("10"), String::from("1f603"));
-    mapping.insert(String::from("100"), String::from("1f604"));
-    mapping.insert(String::from("1000"), String::from("1f601"));
-    mapping.insert(String::from("10000"), String::from("1f606"));
+    mapping.insert(1u128, String::from("1f600"));
+    mapping.insert(10u128, String::from("1f603"));
+    mapping.insert(100u128, String::from("1f604"));
+    mapping.insert(1000u128, String::from("1f601"));
+    mapping.insert(10000u128, String::from("1f606"));
     return mapping;
 }
 
-pub fn get_emotes_from_decimal(decimal: u128) -> Option<String> {
+pub fn get_emotes_from_decimal(decimal: u128) -> Vec<String> {
     let mapping = populate_emojis();
 
-    // decimal to binary representation
+    // decimal to binary string representation
     let binary = format!("{:b}", decimal);
-    //let intval = isize::from_str_radix(bin_idx, 2).unwrap();
 
-    println!("{}", binary);
+    let _binary_u128 = binary.parse::<u128>().unwrap_or(0);
 
     let mut emotes: Vec<String> = Vec::new();
-
-    let mut lookup = String::new();
 
     let reversed_binary = binary.chars().rev();
     for (i, bit) in reversed_binary.enumerate() {
         match bit {
             '1' => {
-                let binary_query = format!("1{}", lookup);
-                println!("{}", binary_query);
+                // We check with the index to see how much the binary u128 should be.
+                // If we are at the very first index, then it's `1` we need to query for.
+                // Else we do `10 ^ i` to get our bit representation for the mapping.
+                let binary_query = if i == 0 { 1 } else { 10_u128.checked_pow(i as u32).unwrap_or(u128::default()) };
+
+                match mapping.get(&binary_query) {
+                    Some(val) => emotes.push(val.into()),
+                    None => {
+                        // Something went wrong with querying for emojis.
+                        // Skip for now and don't do anything.
+                        // TODO: maybe throw error in pallet `NotFoundEmote`
+                    }
+                }
             }
             _ => {}
         }
-        lookup.push('0');
     }
 
-    match mapping.get(&binary) {
-        Some(val) => Option::Some(val.into()),
-        None => Option::None,
-    }
+    emotes
 }
-
-// pub fn get_bytewise_emote(emote: &Emoji) -> Option<u8> {
-//     let mapping = populate_emotijs();
-//     let result = mapping.get(emote.as_str());
-
-//     return match result {
-//         Some(&val) => Option::Some(val),
-//         None => Option::None,
-//     };
-// }
-
-// pub fn get_emote_from_bytewise(byte: u8) -> Option<Emoji> {
-//     let mapping = populate_emotijs();
-//     let result = mapping.iter().find(|(_, &val)| return val == byte);
-
-//     return match result {
-//         Some(val) => emojis::lookup(val.0),
-//         None => Option::None,
-//     };
-// }
--- src/emotes/tests.rs
+++ src/emotes/tests.rs
@@ -1,39 +1,6 @@
 #[cfg(test)]
 mod tests {
-    //use crate::emotes::emotes::get_emotes;
-    use crate::emotes::emotes::get_emotes_from_decimal;
-
-    use super::*;
-
-    // #[test]
-    // fn get_emote__returns_none_if_invalid_input() {
-    //     let input = "foo";
-
-    //     let result = get_emotes(input);
-    //     assert_eq!(result.is_none(), true)
-    // }
-
-    // #[test]
-    // fn get_emote__returns_none_if_emoji_is_not_supported() {
-    //     let input = "👨🏾‍🦱";
-
-    //     let result = get_emotes(input);
-    //     assert_eq!(result.is_none(), true);
-    // }
-
-    // #[test]
-    // fn get_emote__returns_proper_emoji() {
-    //     let input = "💩";
-
-    //     let result = get_emotes(input);
-
-    //     assert_eq!(result.is_some(), true);
-    //     let emoji = result.unwrap();
-
-    //     println!("{}", emoji);
-
-    //     assert_eq!(emoji.as_str(), "💩");
-    // }
+    use crate::emotes::mapping::get_emotes_from_decimal;
 
     #[test]
     fn simple_binary_test() {
@@ -46,14 +13,17 @@ mod tests {
     }
 
     #[test]
-    fn test_emotes_from_decimal() {
-        let result = get_emotes_from_decimal(1).unwrap_or("0".to_string());
-        assert_eq!(result, "1f600");
+    fn pow_math_test() {
+        assert_eq!(10_u128.checked_pow(2), Some(100));
+    }
 
-        let result = get_emotes_from_decimal(4).unwrap_or("0".to_string());
-        println!("{}", result);
-
-        let result = get_emotes_from_decimal(10).unwrap_or("0".to_string());
-        println!("{}", result);
+    #[test]
+    fn emotes_from_decimal_test() {
+        let result = get_emotes_from_decimal(1);
+        assert_eq!(result, vec!["1f600"]);
+        let result = get_emotes_from_decimal(10);
+        assert_eq!(result, vec!["1f603", "1f601"]);
+        let result = get_emotes_from_decimal(15);
+        assert_eq!(result, vec!["1f600", "1f603", "1f604", "1f601"])
     }
 }
